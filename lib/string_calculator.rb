6faa2cb33ad4b70e15ce7f2fc904e3cfd04bfab6
alisha250302 <alishabaig2002@gmail.com>
2025-10-04 17:17:38 +0530

    Support custom delimiters defined with // at the start of string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,7 +1,14 @@
 class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
-    nums = numbers.split(/,|\n/).map(&:to_i)
+    
+    delimiter = /,|\n/
+    if numbers.start_with?("//")
+      delimiter_line, numbers = numbers.split("\n", 2)
+      delimiter = Regexp.escape(delimiter_line[2..])
+    end
+
+    nums = numbers.split(/#{delimiter}/).map(&:to_i)
     nums.sum
   end
 end
